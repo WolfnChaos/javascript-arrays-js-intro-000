35bcedd37f85ed4c1c38089d346536ab8b7191f0
Bradly Horn <wolfnchaos@gmail.com>
2020-07-28 17:28:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  array[0] = "element"
-  return array
+  array.unshift("element")
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift("element")
-  return array
-}
